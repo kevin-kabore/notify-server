2f32b0fca11f0f72dea60d04592b489255580b68
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-15 23:57:02 -0700

    Adds get to gist and console.log content in react

---
+++ routes/changelog.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var rp = require('request-promise');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  rp({
+    uri: 'https://api.github.com/gists/46cf621bdcf2ed59606d3c2ba0029e70',
+    json: true,
+    headers: {
+      'User-Agent': 'kevin-kabore'
+    }
+  })
+    .then(function(data) {
+      var content = data.files.changelog.content;
+      res.send(content);
+    })
+    .catch(function(err) {
+      res.send(err);
+    });
+});
+
+module.exports = router;
