08f66c8a10715fdbb36bb68d61a6e7231e59af17
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-16 01:50:51 -0700

    fixes get request to api

--- routes/changelog.js
+++ routes/changelog.js
@@ -4,12 +4,13 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   rp({
-    uri: 'https://api.github.com/gists/46cf621bdcf2ed59606d3c2ba0029e70',
-    json: true,
-    headers: {
-      'User-Agent': 'kevin-kabore'
-    }
-  })
+
+      uri: 'https://api.github.com/gists/46cf621bdcf2ed59606d3c2ba0029e70',
+      json: true,
+      headers: {
+        'User-Agent': 'kevin-kabore'
+      }
+    })
     .then(function(data) {
       var content = data.files.changelog.content;
       res.send(content);
