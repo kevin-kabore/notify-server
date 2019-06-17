75e44b5a0f18cdcd12a22753e1b53b2ab7bc1b2e
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-17 02:05:25 -0700

    Removes github access token

--- routes/changelog.js
+++ routes/changelog.js
@@ -2,13 +2,12 @@ var express = require('express');
 var rp = require('request-promise');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/changelog/', function(req, res, next) {
   rp({
     uri: 'https://api.github.com/gists/46cf621bdcf2ed59606d3c2ba0029e70',
     json: true,
     headers: {
-      'User-Agent': 'kevin-kabore',
-      Authorization: 'token 29b35ba2eef5dd5753dc37903b72e43dbb457d4f'
+      'User-Agent': 'kevin-kabore'
     }
   })
     .then(function(data) {
